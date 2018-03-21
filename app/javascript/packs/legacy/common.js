5dbd73f948a8ae4d947fd5b3a6b65af9453d6c8f
Andy Braga <ab@luckypike.com>
2018-03-21 18:34:57 +0300

    Выбор города

--- app/javascript/packs/legacy/common.js
+++ app/javascript/packs/legacy/common.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import Parallax from 'parallax-js';
 
+import Cookies from 'js-cookie';
+
 
 $(function(){
   var _b = $('body');
@@ -20,5 +22,30 @@ $(function(){
   if(_ppl.length > 0) {
     var parallaxInstance = new Parallax(_ppl.get(0));
   }
+
+  var _ref = document.referrer.replace(/^[^:]+:\/\/[^/]+/, '').replace(/#.*/, '');
+  var _wlp = window.location.pathname;
+  if(_ref == '' && Cookies.get('geo') == 'nizhny' && _wlp == '/') window.location.href = '/nizhny';
+  if(_ref == '' && Cookies.get('geo') == 'moscow' && _wlp == '/nizhny') window.location.href = '/';
+
+  if(Cookies.get('geo') == null)  {
+    $('.geo_bg').addClass('act');
+    $('body').addClass('full_screen');
+  }
+
+  $('.geo_sl .geo_ct').on('click', function() {
+    $('body').removeClass('full_screen');
+    Cookies.set('geo', $(this).data('geo'), {
+      expires: 365
+    });
+    if ($(this).hasClass('geo_msk') && _wlp == '/') {
+      $('.geo_bg').removeClass('act');
+      return false;
+    }
+    if ($(this).hasClass('geo_nn') && _wlp == '/nizhny') {
+      $('.geo_bg').removeClass('act');
+      return false;
+    }
+  });
 
 });
