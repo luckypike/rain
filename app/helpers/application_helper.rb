f34d8628bf82f6aa72951e34c01f87f9d3ed508b
Andy Braga <ab@luckypike.com>
2018-03-21 19:19:07 +0300

    Еще немного мета-теги

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     file = File.open(Rails.root.join('app', 'javascript', 'images') + "#{path}", 'rb')
     raw file.read
   end
+
+  def city
+    @city_key == :moscow ? nil : @city_key
+  end
 end
